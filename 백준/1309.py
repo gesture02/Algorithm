1d292dc073e92871f4446fc70081064cf349817e
Kyung Jae Kim <gesture02@gmail.com>
2021-10-06 21:25:13 +0900

    -

---
+++ 백준/1309.py
@@ -0,0 +1,12 @@
+import sys
+
+n = int(sys.stdin.readline().rstrip())
+dp = [[0, 0, 0] for _ in range(100001)]
+dp[1] = [1, 1, 1]
+
+for i in range(2, 100001):
+    dp[i][0] = sum(dp[i-1]) % 9901
+    dp[i][1] = (dp[i-1][0] + dp[i-1][2]) % 9901
+    dp[i][2] = (dp[i-1][0] + dp[i-1][1]) % 9901
+
+print(sum(dp[n]) % 9901)
