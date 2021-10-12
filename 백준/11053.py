6b0cf8e197e675a08a266b2f5b774f803a4dac12
Kyung Jae Kim <gesture02@gmail.com>
2021-10-12 21:29:03 +0900

    -

---
+++ 백준/11053.py
@@ -0,0 +1,13 @@
+import sys
+
+n = int(sys.stdin.readline().rstrip())
+input = list(map(int, sys.stdin.readline().split()))
+dp = [1] * n
+
+for i in range(1, n):
+    for j in range(i):
+        if input[i] > input[j]:
+            if dp[i] < dp[j] + 1:
+                dp[i] = dp[j] + 1
+
+print(max(dp))
