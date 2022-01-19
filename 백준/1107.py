28b6c64ac8b0dec294542aa0aadfe93b94d2fadd
gesture02 <gesture02@gmail.com>
2022-01-19 17:44:34 +0900

    -

--- 백준/1107.py
+++ 백준/1107.py
@@ -1,20 +1,55 @@
 import sys
 
-n = int(sys.stdin.readline().rstrip())
-m = int(sys.stdin.readline().rstrip())
-input = list(map(int, sys.stdin.readline().split()))
-
-def check(num):
-    num = str(num)
-    for i in num:
-        if int(i) in input:
+def check(number):
+    n = str(number)
+    for i in n:
+        if i in inputs:
             return False
     return True
+
+n = int(sys.stdin.readline().rstrip())
+m = int(sys.stdin.readline().rstrip())
+inputs = list(sys.stdin.readline().split())
 
 result = abs(n - 100)
 
 for i in range(1000001):
     if check(i):
-        result = min(result, len(str(i)) + abs(i - n))
+        result = min(result, len(str(i)) + abs(i-n))
 
 print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def check(num):
+#     num = str(num)
+#     for i in num:
+#         if int(i) in inputs:
+#             return False
+#     return True
+#
+# result = abs(n - 100)
+#
+# for i in range(1000001):
+#     if check(i):
+#         result = min(result, len(str(i)) + abs(i - n))
+#
+# print(result)
