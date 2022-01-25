d381a4c5f4742bff52ccc762d55c3fce622ed4a8
gesture02 <gesture02@gmail.com>
2022-01-25 18:05:11 +0900

    -

--- 백준/11053.py
+++ 백준/11053.py
@@ -1,12 +1,12 @@
 import sys
 
 n = int(sys.stdin.readline().rstrip())
-input = list(map(int, sys.stdin.readline().split()))
+inputs = list(map(int, sys.stdin.readline().split()))
 dp = [1] * n
 
-for i in range(1, n):
+for i in range(n):
     for j in range(i):
-        if input[i] > input[j]:
+        if inputs[i] > inputs[j]:
             if dp[i] < dp[j] + 1:
                 dp[i] = dp[j] + 1
 
