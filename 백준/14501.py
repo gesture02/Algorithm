6e97a1c6864781f240ea7848fb7340e1f90f266d
gesture02 <gesture02@gmail.com>
2022-01-25 17:03:10 +0900

    -

--- 백준/14501.py
+++ 백준/14501.py
@@ -1,12 +1,35 @@
 import sys
 
-n = int(sys.stdin.readline().rstrip())
-input = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
-dp = [0] * (n+1)
-for i in range(n-1, -1, -1):
-    if i + input[i][0] > n:
-        dp[i] = dp[i + 1]
-    else:
-        dp[i] = max(dp[i + 1], input[i][1] + dp[i + input[i][0]])
+# n = int(sys.stdin.readline().rstrip())
+# input = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+# dp = [0] * (n+1)
+# for i in range(n-1, -1, -1):
+#     if i + input[i][0] > n:
+#         dp[i] = dp[i + 1]
+#     else:
+#         dp[i] = max(dp[i + 1], input[i][1] + dp[i + input[i][0]])
+#
+# print(max(dp))
 
-print(max(dp))
+
+#재귀로 풀기
+
+def solve(day, money):
+    global answer
+
+    if day == n:
+        if answer < money:
+            answer = money
+        return
+    if day > n:
+        return
+
+    solve(day+inputs[day][0], money+inputs[day][1])
+    solve(day+1, money)
+n = int(sys.stdin.readline().rstrip())
+inputs = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+answer = 0
+
+solve(0, 0)
+
+print(answer)
