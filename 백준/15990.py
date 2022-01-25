f909bd7a4b446023e2a8bc27e0cf3baba8e1e4c2
gesture02 <gesture02@gmail.com>
2022-01-25 17:03:03 +0900

    -

---
+++ 백준/15990.py
@@ -0,0 +1,17 @@
+import sys
+
+dp = [[0, 0, 0] for _ in range(10001)]
+dp[1] = [1, 0, 0]
+dp[2] = [0, 1, 0]
+dp[3] = [1, 1, 1]
+for i in range(4, 10001):
+    dp[i][0] = (dp[i-1][1] + dp[i-1][2]) % 1000000009
+    dp[i][1] = (dp[i-2][0] + dp[i-2][2]) % 1000000009
+    dp[i][2] = (dp[i-3][0] + dp[i-3][1]) % 1000000009
+
+a = int(sys.stdin.readline().rstrip())
+
+for _ in range(a):
+    n = int(sys.stdin.readline().rstrip())
+
+    print(sum(dp[n]) % 1000000009)
