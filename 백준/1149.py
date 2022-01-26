3d9a4ca613845fecbb8e96c73526d6f089b58a75
gesture02 <gesture02@gmail.com>
2022-01-26 18:35:17 +0900

    -

--- 백준/1149.py
+++ 백준/1149.py
@@ -1,13 +1,14 @@
 import sys
 
 n = int(sys.stdin.readline().rstrip())
-input = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+inputs = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+
 dp = [[0, 0, 0] for _ in range(n)]
-dp[0] = input[0]
+dp[0] = inputs[0]
 
 for i in range(1, n):
-    dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + input[i][0]
-    dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + input[i][1]
-    dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + input[i][2]
+    dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + inputs[i][0]
+    dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + inputs[i][1]
+    dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + inputs[i][2]
 
 print(min(dp[n-1]))
