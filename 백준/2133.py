42ad7ada175516009ec3e141978f604bdc44887f
gesture02 <gesture02@gmail.com>
2022-01-26 20:29:11 +0900

    -

--- 백준/2133.py
+++ 백준/2133.py
@@ -1,13 +1,13 @@
 import sys
 
+dp = [0] * (31)
+dp[0] = 1
+dp[2] = 3
+for i in range(4, 31, 2):
+    dp[i] = 3 * dp[i-2]
+    for j in range(4, i+1, 2):
+        dp[i] += 2 * dp[i-j]
+
+
 n = int(sys.stdin.readline().rstrip())
-dp = [0] * (n+1)
-if n > 1:
-    dp[2] = 3
-
-for i in range(4, n+1, 2):
-    dp[i] = 3 * dp[i-2] + 2
-    for j in range(2, i, 2):
-        dp[i] += dp[j-2] * 2
-
 print(dp[n])
