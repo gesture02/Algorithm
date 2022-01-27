1cec7aac7320cfa0eb9eb48abe2da978cb77e800
gesture02 <gesture02@gmail.com>
2022-01-27 20:29:15 +0900

    -

--- 백준/10845.py
+++ 백준/10845.py
@@ -1,34 +1,32 @@
 import sys
 from collections import deque
 
-n = int(sys.stdin.readline().rstrip())
+a = int(sys.stdin.readline().rstrip())
+q = deque()
+for _ in range(a):
+    op = sys.stdin.readline().split()
 
-d = deque()
-
-for _ in range(n):
-    input = sys.stdin.readline().split()
-
-    if input[0] == 'push':
-        d.append(input[1])
-    elif input[0] == 'pop':
-        if d:
-            print(d.popleft())
+    if op[0] == 'push':
+        q.append(int(op[1]))
+    elif op[0] == 'front':
+        if q:
+            print(q[0])
         else:
             print(-1)
-    elif input[0] == 'size':
-        print(len(d))
-    elif input[0] == 'empty':
-        if d:
+    elif op[0] == 'back':
+        if q:
+            print(q[-1])
+        else:
+            print(-1)
+    elif op[0] == 'empty':
+        if q:
             print(0)
         else:
             print(1)
-    elif input[0] == 'front':
-        if d:
-            print(d[0])
+    elif op[0] == 'pop':
+        if q:
+            print(q.popleft())
         else:
             print(-1)
-    elif input[0] == 'back':
-        if d:
-            print(d[-1])
-        else:
-            print(-1)
+    elif op[0] == 'size':
+        print(len(q))
