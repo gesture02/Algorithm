a0fd04e06119f7a9b8160c0f05095c96336e2472
gesture02 <gesture02@gmail.com>
2022-01-27 20:29:20 +0900

    -

--- 백준/10819.py
+++ 백준/10819.py
@@ -1,28 +1,37 @@
 import sys
-def dfs():
-    global maxv
 
-    if len(s) == n:
-        sums = 0
-        for i in range(n-1):
-            sums += abs(s[i] - s[i+1])
-        maxv = max(maxv, sums)
-        return
+def nextPermutation(p):
+    i = n-1
+    while i > 0 and p[i-1] >= p[i]:
+        i -= 1
+    if i <= 0: return False
+    j = n-1
+    while p[i-1] >= p[j]:
+        j -= 1
+    p[i-1], p[j] = p[j], p[i-1]
+    j = n-1
+    while i < j:
+        p[i], p[j] = p[j], p[i]
+        i += 1
+        j -= 1
+    return True
 
-    for i in range(n):
-        if not visited[i]:
-            visited[i] = 1
-            s.append(input[i])
-            dfs()
-            visited[i] = 0
-            s.pop()
-
+def permutation(inputs):
+    global answer
+    s = 0
+    for i in range(n - 1):
+        s += abs(inputs[i] - inputs[i + 1])
+    answer = s
+    while nextPermutation(inputs):
+        s = 0
+        for a in range(n - 1):
+            s += abs(inputs[a] - inputs[a + 1])
+        if answer < s:
+            answer = s
 n = int(sys.stdin.readline().rstrip())
-input = list(map(int, sys.stdin.readline().split()))
-visited = [0] * n
-s = []
-maxv = 0
+inputs = list(map(int, sys.stdin.readline().split()))
+answer = -sys.maxsize
+inputs.sort()
 
-dfs()
-
-print(maxv)
+permutation(inputs)
+print(answer)
