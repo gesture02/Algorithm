3ec41e4d13715e22cf2d6530adf0f612ced2ee17
gesture02 <gesture02@gmail.com>
2022-01-30 00:20:25 +0900

    -

--- 백준/1182.py
+++ 백준/1182.py
@@ -1,13 +1,15 @@
 import sys
 
-n = int(sys.stdin.readline().rstrip())
-input = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
-dp = [0] * (n+1)
+n, m = map(int, sys.stdin.readline().split())
+inputs = list(map(int, sys.stdin.readline().split()))
+cnt = 0
+for i in range(1,  1 << n):
+    sums = 0
+    for j in range(n):
+        if i & (1 << j):
+            sums += inputs[j]
 
-for i in range(n-1, -1, -1):
-    if input[i][0] + i > n:
-        dp[i] = dp[i+1]
-    else:
-        dp[i] = max(dp[i+1], dp[input[i][0] + i] + input[i][1])
+    if sums == m:
+        cnt += 1
 
-print(max(dp))
+print(cnt)
