f0d9a30fe818f38d2087c90edaf7d8a04cd33dc3
gesture02 <gesture02@gmail.com>
2022-02-03 17:50:44 +0900

    재귀로 풀어보기

--- 백준/1182.py
+++ 백준/1182.py
@@ -1,15 +1,29 @@
 import sys
 
+def solve(sum, index):
+    global cnt
+
+    if index == n:
+        if sum == m:
+            cnt += 1
+        return
+
+    solve(sum+inputs[index], index+1)
+    solve(sum, index+1)
+
+
 n, m = map(int, sys.stdin.readline().split())
 inputs = list(map(int, sys.stdin.readline().split()))
 cnt = 0
-for i in range(1,  1 << n):
-    sums = 0
-    for j in range(n):
-        if i & (1 << j):
-            sums += inputs[j]
+# for i in range(1,  1 << n):
+#     sums = 0
+#     for j in range(n):
+#         if i & (1 << j):
+#             sums += inputs[j]
+#
+#     if sums == m:
+#         cnt += 1
+#print(cnt)
 
-    if sums == m:
-        cnt += 1
-
-print(cnt)
+solve(0, 0)
+print(cnt-1 if m == 0 else cnt)
