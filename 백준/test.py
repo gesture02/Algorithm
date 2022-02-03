b4e5f07ee254d53bcdac8fe8fde9a981f8d038bb
gesture02 <gesture02@gmail.com>
2022-02-03 15:52:13 +0900

    -

--- 백준/test.py
+++ 백준/test.py
@@ -1,35 +1,43 @@
 import sys
 input = sys.stdin.readline
 
-dx = [0, -1, 0, 1]
-dy = [1, 0, -1, 0]
+def check(a, l):
+    c = [0] * n
 
-n = int(input().rstrip())
-c = [[0] * 101 for _ in range(101)]
+    for i in range(1, n):
+        if a[i-1] != a[i]:
+            diff = abs(a[i]-a[i-1])
+            if diff != 1:
+                return False
 
-for _ in range(n):
-    y, x, d, g = map(int, input().split())
-    c[x][y] = 1
-    dir = [d]
+            if a[i-1] < a[i]:
+                for j in range(1, l+1):
+                    if i-j < 0: return False
+                    if a[i-1] != a[i-j]: return False
+                    if c[i-j]: return False
+                    c[i-j] = 1
 
-    for i in range(1, g+1):
-        tmp = dir[:]
-        tmp = tmp[::-1]
+            elif a[i-1] > a[i]:
+                for j in range(l):
+                    if i+j >= n: return False
+                    if a[i] != a[i+j]: return False
+                    if c[i+j]: return False
+                    c[i+j] = 1
+    return True
 
-        for j in range(len(tmp)):
-            tmp[j] = (tmp[j] + 1) % 4
-
-        dir += tmp
-
-    for d in dir:
-        x += dx[d]
-        y += dy[d]
-        c[x][y] = 1
-
+n, l = map(int, input().split())
+inputs = [list(map(int, input().split())) for _ in range(n)]
 cnt = 0
-for i in range(100):
-    for j in range(100):
-        if c[i][j] and c[i+1][j] and c[i][j+1] and c[i+1][j+1]:
-            cnt += 1
 
+for i in range(n):
+    a = inputs[i][:]
+    if check(a, l):
+        cnt += 1
+
+for i in range(n):
+    a = []
+    for j in range(n):
+        a.append(inputs[j][i])
+    if check(a, l):
+        cnt += 1
 print(cnt)
