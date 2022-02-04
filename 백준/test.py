57e745e581d66b43df9b4ee4d7d3c1f4f26a6a64
gesture02 <gesture02@gmail.com>
2022-02-05 00:34:03 +0900

    순열로 풀어보기

--- 백준/test.py
+++ 백준/test.py
@@ -1,43 +1,33 @@
 import sys
 input = sys.stdin.readline
 
-def check(a, l):
-    c = [0] * n
+def check(index, p, k):
+    if inputs[index] == '<':
+        if p[-1] > k:
+            return False
+    else:
+        if p[-1] < k:
+            return False
 
-    for i in range(1, n):
-        if a[i-1] != a[i]:
-            diff = abs(a[i]-a[i-1])
-            if diff != 1:
-                return False
-
-            if a[i-1] < a[i]:
-                for j in range(1, l+1):
-                    if i-j < 0: return False
-                    if a[i-1] != a[i-j]: return False
-                    if c[i-j]: return False
-                    c[i-j] = 1
-
-            elif a[i-1] > a[i]:
-                for j in range(l):
-                    if i+j >= n: return False
-                    if a[i] != a[i+j]: return False
-                    if c[i+j]: return False
-                    c[i+j] = 1
     return True
 
-n, l = map(int, input().split())
-inputs = [list(map(int, input().split())) for _ in range(n)]
-cnt = 0
+def solve(index, p):
+    if index == n+1:
+        result.append(''.join(map(str, p)))
+        return
 
-for i in range(n):
-    a = inputs[i][:]
-    if check(a, l):
-        cnt += 1
+    for i in range(10):
+        if c[i]: continue
+        if index == 0 or check(index-1, p, i):
+            c[i] = 1
+            solve(index+1, p+[i])
+            c[i] = 0
 
-for i in range(n):
-    a = []
-    for j in range(n):
-        a.append(inputs[j][i])
-    if check(a, l):
-        cnt += 1
-print(cnt)
+n =  int(input().rstrip())
+inputs = list(input().split())
+c = [0] * 10
+result = []
+solve(0, [])
+result.sort()
+print(result[-1])
+print(result[0])
