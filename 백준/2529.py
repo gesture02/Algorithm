fdc2cb36251a1795f66cf72a4e4a0cef8dabbd14
gesture02 <gesture02@gmail.com>
2022-02-04 21:49:11 +0900

    순열로 풀어보기

--- 백준/2529.py
+++ 백준/2529.py
@@ -1,32 +1,94 @@
 import sys
-def dfs(s, depth):
-    global result
-    if len(s) == n+1:
-        result.append(''.join(map(str, s)))
-        return
-    for i in range(10):
-        if not visited[i]:
-            if input[depth] == '<':
-                if s[-1] > i:
-                    continue
-            if input[depth] == '>':
-                if s[-1] < i:
-                    continue
-            visited[i] = 1
-            s.append(i)
-            dfs(s, depth+1)
-            visited[i] = 0
-            s.pop()
 
+# def ok(numbers, num, index):
+#     if inputs[index] == '<':
+#         if numbers[-1] > num:
+#             return False
+#     if inputs[index] == '>':
+#         if numbers[-1] < num:
+#             return False
+#
+#     return True
+#
+# def solve(numbers, index):
+#     if index == n+1:
+#         answer.append(''.join(map(str, numbers)))
+#         return
+#
+#     for i in range(10):
+#         if check[i]: continue
+#         check[i] = 1
+#         if index == 0 or ok(numbers, i, index-1):
+#             solve(numbers+[i], index+1)
+#         check[i] = 0
+#
+#
+# n = int(sys.stdin.readline().rstrip())
+# inputs = list(sys.stdin.readline().split())
+# check = [0] * 10
+# answer = []
+#
+# solve([], 0)
+#
+# print(answer[-1])
+# print(answer[0])
+
+def nextPermutation(p):
+    i = len(p) - 1
+    while i > 0 and p[i-1] >= p[i]:
+        i -= 1
+    if i <= 0: return False
+    j = len(p)-1
+    while p[i-1] >= p[j]:
+        j -= 1
+    p[i-1], p[j] = p[j], p[i-1]
+    j = len(p)-1
+    while i < j:
+        p[i], p[j] = p[j], p[i]
+        i += 1
+        j -= 1
+    return True
+
+def prevPermutation(p):
+    i = len(p) - 1
+    while i > 0 and p[i-1] <= p[i]:
+        i -= 1
+    if i <= 0: return False
+    j = len(p)-1
+    while p[i-1] <= p[j]:
+        j -= 1
+    p[i-1], p[j] = p[j], p[i-1]
+    j = len(p)-1
+    while i < j:
+        p[i], p[j] = p[j], p[i]
+        i += 1
+        j -= 1
+    return True
+
+def check(p):
+    for i in range(n):
+        if inputs[i] == '<':
+            if p[i] > p[i+1]:
+                return False
+        else:
+            if p[i] < p[i+1]:
+                return False
+    return True
 n = int(sys.stdin.readline().rstrip())
-input = list(sys.stdin.readline().split())
-visited = [0] * 10
-result = []
+inputs = list(sys.stdin.readline().split())
 
-for i in range(10):
-    visited[i] = 1
-    dfs([i], 0)
-    visited[i] = 0
+a = [i for i in range(9, 9-n-1, -1)]
+b = [i for i in range(n+1)]
 
-print(result[-1])
-print(result[0])
+while True:
+    if check(a):
+        print(''.join(map(str, a)))
+        break
+    if not prevPermutation(a):
+        break
+while True:
+    if check(b):
+        print(''.join(map(str, b)))
+        break
+    if not nextPermutation(b):
+        break
