4827426f452da0589edc72bb080bc8819cc0f644
gesture02 <gesture02@gmail.com>
2022-02-19 00:05:44 +0900

    -

--- 백준/2156.py
+++ 백준/2156.py
@@ -1,13 +1,12 @@
 import sys
+a = int(sys.stdin.readline().rstrip())
+inputs = [0]
+inputs += [int(sys.stdin.readline().rstrip()) for _ in range(a)]
+dp = [[0, 0, 0] for _ in range(a+1)]
 
-n = int(sys.stdin.readline().rstrip())
-input = [0]
-input += [int(sys.stdin.readline().rstrip()) for _ in range(n)]
-dp = [0] * (n+1)
-dp[1] = input[1]
-if n > 1:
-    dp[2] = dp[1] + input[2]
-for i in range(3, n+1):
-    dp[i] = max(dp[i-1], dp[i-2] + input[i], dp[i-3] + input[i-1] + input[i])
-
-print(dp[n])
+for i in range(1, a+1):
+    dp[i][0] = max(dp[i-1])
+    dp[i][1] = dp[i-1][0] + inputs[i]
+    dp[i][2] = dp[i-1][1] + inputs[i]
+    
+print(max(dp[a]))
