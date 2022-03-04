cec162fc68cb12bc5da5a0c3caeb50665e5447b2
gesture02 <gesture02@gmail.com>
2022-03-04 19:34:48 +0900

    -

--- 백준/11055.py
+++ 백준/11055.py
@@ -1,14 +1,13 @@
 import sys
 
 n = int(sys.stdin.readline().rstrip())
-input = list(map(int, sys.stdin.readline().split()))
-dp = [0] * n
-dp[0] = input[0]
+inputs = list(map(int, sys.stdin.readline().split()))
+dp = [i for i in inputs]
 
-for i in range(1, n):
-    dp[i] = input[i]
+for i in range(n):
     for j in range(i):
-        if input[i] > input[j]:
-            if dp[i] < dp[j] + input[i]:
-                dp[i] = dp[j] + input[i]
+        if inputs[i] > inputs[j]:
+            if dp[i] < dp[j] + inputs[i]:
+                dp[i] = dp[j] + inputs[i]
+
 print(max(dp))
