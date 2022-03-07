e8182c03e0a054fd4f3cb43e25b2b5ff7c96c81f
gesture02 <gesture02@gmail.com>
2022-03-07 16:39:34 +0900

    -

--- 백준/11723.py
+++ 백준/11723.py
@@ -4,19 +4,19 @@ n = int(sys.stdin.readline().rstrip())
 answer = 0
 
 for _ in range(n):
-    input = sys.stdin.readline().split()
-    if input[0] == 'add':
-        answer = answer | (1 << int(input[1]))
-    if input[0] == 'remove':
-        answer = answer & ~(1 << int(input[1]))
-    if input[0] == 'check':
-        if answer & (1 << int(input[1])):
+    op = list(sys.stdin.readline().split())
+    if op[0] == 'add':
+        answer = answer | (1 << int(op[1]))
+    elif op[0] == 'remove':
+        answer = answer & ~(1 << int(op[1]))
+    elif op[0] == 'check':
+        if answer & (1 << int(op[1])):
             print(1)
         else:
             print(0)
-    if input[0] == 'toggle':
-        answer = answer ^ (1 << int(input[1]))
-    if input[0] == 'all':
-        answer = (1 << 21) -1
-    if input[0] == 'empty':
+    elif op[0] == 'toggle':
+        answer = answer ^ (1 << int(op[1]))
+    elif op[0] == 'all':
+        answer = (1 << 21) - 1
+    elif op[0] == 'empty':
         answer = 0
