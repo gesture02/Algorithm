d8896f120275a1a4df28e14e9919b049d11368f2
gesture02 <gesture02@gmail.com>
2022-03-10 23:36:15 +0900

    -

--- 백준/13549.py
+++ 백준/13549.py
@@ -1,6 +1,7 @@
 import sys
 from collections import deque
 
+
 def bfs(v):
     q = deque()
     q.append(v)
@@ -9,17 +10,17 @@ def bfs(v):
     while q:
         w = q.popleft()
 
-        for nx in (w-1, w+1, w*2):
-            if 0 <= nx <= 100000 and dist[nx] == -1:
-                if nx == w*2:
-                    dist[nx] = dist[w]
-                    q.appendleft(nx)
-                else:
-                    dist[nx] = dist[w] + 1
-                    q.append(nx)
+        for i in (w+1, w-1, 2*w):
+            if 0 <= i <= 200000 and dist[i] == -1:
+                if i == w+1 or i == w-1:
+                    dist[i] = dist[w] + 1
+                    q.append(i)
+                if i == 2*w:
+                    dist[i] = dist[w]
+                    q.appendleft(i)
+
 n, m = map(int, sys.stdin.readline().split())
-dist = [-1] * 100001
-path = [0] * 100001
-result = []
+dist = [-1] * 200001
+
 bfs(n)
 print(dist[m])
